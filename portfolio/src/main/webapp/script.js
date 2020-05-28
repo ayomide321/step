41fafce2506a833878f4bde024e158d0eaa5e156
Ayomide Omolewa <ayomideomolewa@gmail.com>
2020-05-28 13:35:39 -0500

    CREATED BASIC BACKBONE OF WEBSITE

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -22,7 +22,51 @@ function addRandomGreeting() {
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
-  // Add it to the page.
+
+        {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+}
+
+function testkey() {
+    var inputVal = document.getElementById("myInput").value;
+    if(inputVal == "I am Justice I am God" || "i am justice i am God" || "I am justice I am kira" || "I am justice, I am God")
+        {
+            window.alert("You got it right! My favorite TV show is Deathnote!");
+        }
+    else
+        {
+            window.alert("Try again, you almost got it!");
+        }
+}
+/*
+function pageLoad() {
+    var elem = document.getElementById("barPercent");
+    width = 1;
+    elem.style.width = width + '%';
+    elem.innerHTML = width * 1 + '%';
+}
+function load() {
+    var elem = document.getElementById("barPercent");
+    var inputVal = document.getElementById("myInput").value;
+    var width = 1;
+    var id = setInterval(frameElement,10);
+    function frame() {
+        if(inputVal = "I am Justice I am God" || "i am justice i am God" || "I am justice I am kira" || "I am justice, I am God")
+        {
+            if(width>= elem.style.width) {
+                clearInterval(id);
+            }
+            else
+            {
+                width++;
+                elem.style.width = width + '%';
+                elem.innerHTML = width * 1 + '%';
+            }
+        }
+    }
+}
+
+//Javascript wasn't working for what I wanted it to so had to scrap original idea as I ran out of time
+*/
